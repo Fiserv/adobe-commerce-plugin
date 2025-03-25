f5abc25f0fc608ca9cf10cadbf7fab6d4a96d54e
Ashwini Bhave - 10225817 <ashwini.bhave@fiserv.com>
2025-03-25 13:42:45 +0000

    COM-19155

--- Model/Config/CommerceHub/ConfigProvider.php
+++ Model/Config/CommerceHub/ConfigProvider.php
@@ -24,6 +24,8 @@ class ConfigProvider implements ConfigProviderInterface
 	const IS_ACTIVE_KEY = 'isActive';
 	const IS_PAYMENT_ACTIVE_KEY = 'isPaymentActive';
 	const MERCHANT_ID_KEY = 'merchantId';
+	const SHOW_PRIVACY_STATEMENT_KEY = "show_privacy_statement";
+	const KEY_PRIVACY_NOTICE_LINK = "privacy_notice_link";
 	const API_KEY_KEY = 'apiKey';
 	const CC_MAPPER_KEY = 'ccTypesMapper';
 	const ENV_KEY = 'environment';
@@ -80,6 +82,8 @@ class ConfigProvider implements ConfigProviderInterface
 		$config = [
 			self::IS_ACTIVE_KEY => $this->config->isActive($storeId),
 			self::IS_PAYMENT_ACTIVE_KEY => $this->config->isPaymentActive($storeId),
+			self::SHOW_PRIVACY_STATEMENT_KEY => $this->config->showPrivacyStatement($storeId),
+			self::KEY_PRIVACY_NOTICE_LINK => $this->config->getPrivacyNoticeLink($storeId),
 			self::MERCHANT_ID_KEY => $this->config->getMerchantId($storeId),
 			self::API_KEY_KEY => $this->config->getApiKey(),
 			self::CC_MAPPER_KEY => $this->config->getCcTypesMapper(),
