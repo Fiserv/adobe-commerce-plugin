64b89e344e3247696dbb632093f20b3751ca0475
Tyson Nguyen - 10201988 <tyson.nguyen@fiserv.com>
2025-05-08 05:20:11 +0000

    MCH-12043 Adding Filtering to Order Page.

--- view/adminhtml/web/js/view/declines/failed-transaction.js
+++ view/adminhtml/web/js/view/declines/failed-transaction.js
@@ -1,31 +1,66 @@
 define([
 	'jquery',
+	'jquery/ui'
 ], function ($) {
-
 	var currentPage = 1;
 	var rowsPerPage = 20;
+	var originalData = [];
+
+	function storeOriginalData() {
+		$('#transactionsTable tbody tr').each(function () {
+			originalData.push($(this).clone());
+		});
+	}
+
+	function applyFiltersAndSort() {
+		var stateFilter = $('#transactionStateFilter').val().toUpperCase();
+		var statusFilter = $('#approvalStatus').val().toUpperCase();
+		var searchInput = $('#searchInput').val().toUpperCase();
+		var startDate = $('#startDate').val();
+		var endDate = $('#endDate').val();
+
+		var filteredData = originalData.filter(function ($row) {
+			var rowState = $row.find('td').eq(2).text().toUpperCase();
+			var rowStatus = $row.find('td').eq(3).text().toUpperCase();
+			var dateText = $row.find('td').eq(0).text();
+			var rowDate = new Date(dateText);
+			var containsSearch = false;
+			$row.find('td').each(function () {
+				var txtValue = $(this).text();
+				if (txtValue.toUpperCase().indexOf(searchInput) > -1) {
+					containsSearch = true;
+					return false;
+				}
+			});
+			var dateInRange = (!startDate || new Date(startDate) <= rowDate) && (!endDate || rowDate <= new Date(endDate));
+			return (stateFilter === "" || rowState === stateFilter) && (statusFilter === "" || rowStatus === statusFilter) && (searchInput === "" || containsSearch) && dateInRange;
+		});
+
+		updateTableBody(filteredData);
+		setupPagination();
+		toggleResetButton();
+	}
+
+	function updateTableBody(data) {
+		$('#transactionsTable tbody').empty().append(data);
+		setupPagination();
+		displayTable(currentPage);
+	}
 
 	function displayTable(page) {
 		var start = (page - 1) * rowsPerPage;
 		var end = page * rowsPerPage;
 		var visibleRowIndex = 0;
-		var searchInput = $('#searchInput').val().toUpperCase();
 		var noRecordsFound = true;
 
-		$('#transactionsTable tr').each(function(index) {
-			if (index === 0) return; // Skip header row
-			var $row = $(this);
-			if (searchInput === "" || $row.attr('searchedRow') === 'true') {
-				if (visibleRowIndex >= start && visibleRowIndex < end) {
-					$row.addClass('visible').removeClass('hidden');
-					noRecordsFound = false;
-				} else {
-					$row.addClass('hidden').removeClass('visible');
-				}
-				visibleRowIndex++;
+		$('#transactionsTable tbody tr').each(function (index) {
+			if (visibleRowIndex >= start && visibleRowIndex < end) {
+				$(this).show();
+				noRecordsFound = false;
 			} else {
-				$row.addClass('hidden').removeClass('visible');
+				$(this).hide();
 			}
+			visibleRowIndex++;
 		});
 
 		if (noRecordsFound) {
@@ -36,20 +71,12 @@ define([
 	}
 
 	function setupPagination() {
-		var visibleRowCount = 0;
-
-		$('#transactionsTable tr').each(function(index) {
-			if (index === 0) return; // Skip header row
-			if ($(this).attr('searchedRow') === 'true' || $('#searchInput').val() === "") {
-				visibleRowCount++;
-			}
-		});
-
+		var visibleRowCount = $('#transactionsTable tbody tr').length;
 		var pageCount = Math.ceil(visibleRowCount / rowsPerPage);
 		var $paginationControls = $('#paginationControls');
 		$paginationControls.empty();
 
-		var $prevButton = $('<span>').text('<').addClass('pagination-button').on('click', function() {
+		var $prevButton = $('<span>').text('<').addClass('pagination-button').on('click', function () {
 			if (currentPage > 1) {
 				changePage(currentPage - 1);
 			}
@@ -62,7 +89,7 @@ define([
 		var $pageInfo = $('<span>').text(currentPage + ' of ' + pageCount).addClass('pagination-info');
 		$paginationControls.append($pageInfo);
 
-		var $nextButton = $('<span>').text('>').addClass('pagination-button').on('click', function() {
+		var $nextButton = $('<span>').text('>').addClass('pagination-button').on('click', function () {
 			if (currentPage < pageCount) {
 				changePage(currentPage + 1);
 			}
@@ -72,52 +99,94 @@ define([
 		}
 		$paginationControls.append($nextButton);
 
-		// Update the pagination info at the top
 		$('#paginationInfoTop').text(currentPage + ' of ' + pageCount);
 	}
 
 	function changePage(page) {
 		currentPage = page;
 		displayTable(page);
-		setupPagination(); // Update pagination info
+		setupPagination();
 	}
 
 	function changeRowsPerPage() {
 		rowsPerPage = parseInt($('#rowsPerPage').val());
-		currentPage = 1; // Reset current page to 1 when rows per page is changed
-		setupPagination();
-		displayTable(1);
+		currentPage = 1;
+		applyFiltersAndSort();
 	}
 
 	function searchTable() {
-		var filter = $('#searchInput').val().toUpperCase();
-
-		$('#transactionsTable tr').each(function(index) {
-			if (index === 0) return; // Skip header row
-			var $row = $(this);
-			$row.attr('searchedRow', 'false');
-			$row.find('td').each(function() {
-				var txtValue = $(this).text();
-				if (txtValue.toUpperCase().indexOf(filter) > -1) {
-					$row.attr('searchedRow', 'true');
-					return false; // Stop searching further cells in this row
-				}
-			});
-		});
-
-		currentPage = 1; // Reset current page to 1 when search is applied
-
-		requestAnimationFrame(function() {
-			setupPagination();
-			displayTable(1);
+		currentPage = 1;
+		requestAnimationFrame(function () {
+			applyFiltersAndSort();
 		});
 	}
 
-	$(document).ready(function() {
+	function resetFilters() {
+		$('#startDate').val('');
+		$('#endDate').val('');
+		$('#searchInput').val('');
+		$('#transactionStateFilter').val('');
+		$('#approvalStatus').val('');
+		currentPage = 1;
+		applyFiltersAndSort();
+		hideResetButton();
+	}
+
+	function toggleResetButton() {
+		if ($('#searchInput').val() || $('#transactionStateFilter').val() || $('#approvalStatus').val() || $('#startDate').val() || $('#endDate').val()) {
+			$('#resetFilters').show();
+		} else {
+			$('#resetFilters').hide();
+		}
+	}
+
+	function hideResetButton() {
+		$('#resetFilters').hide();
+	}
+
+	function handleDatePickers() {
+		var startDatePicker = $('#startDate').datepicker({
+			dateFormat: 'yy-mm-dd',
+			onSelect: function (selectedDate) {
+				var endDatePicker = $('#endDate');
+				endDatePicker.datepicker('option', 'minDate', selectedDate);
+				applyFiltersAndSort();
+			}
+		});
+		var endDatePicker = $('#endDate').datepicker({
+			dateFormat: 'yy-mm-dd',
+			onSelect: function (selectedDate) {
+				var startDatePicker = $('#startDate');
+				startDatePicker.datepicker('option', 'maxDate', selectedDate);
+				applyFiltersAndSort();
+			}
+		});
+	}
+
+	$(document).ready(function () {
+		storeOriginalData();
+
+		handleDatePickers();
+
 		$('#searchInput').on('keyup', searchTable);
 		$('#rowsPerPage').on('change', changeRowsPerPage);
+
+		$('#transactionStateFilter').on('change', function () {
+			$('option.selected-dropdown').removeClass('selected-dropdown');
+			$(this).find('option:selected').addClass('selected-dropdown');
+			applyFiltersAndSort();
+		});
+
+		$('#approvalStatus').on('change', function () {
+			$('option.selected-dropdown').removeClass('selected-dropdown');
+			$(this).find('option:selected').addClass('selected-dropdown');
+			applyFiltersAndSort();
+		});
+
+		$('#resetFilters').on('click', resetFilters);
+
+		toggleResetButton();
 		setupPagination();
 		displayTable(1);
 	});
-
 });
