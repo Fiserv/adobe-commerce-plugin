1f54d8ed354de955136934bd3456338275d0f239
Eric Margules - 10109724 <eric.margules@fiserv.com>
2025-07-07 14:48:00 +0000

    MCH-2047 - Enable 3DS flow for payment cards

--- view/base/requirejs-config.js
+++ view/base/requirejs-config.js
@@ -14,7 +14,7 @@ var config = {
 		}
 	},
     paths : {
-		SDCv2Library : 'https://commercehub-secure-data-capture.fiservapps.com/3.1.21/checkout',
+		SDCv2Library : 'https://commercehub-secure-data-capture.fiservapps.com/3.2.7/checkout',
 		commercehubQaClient : 'https://qa.api.fiservapps.com/ch/sdk/v1/commercehub-client-sdk',
 		commercehubCertClient : 'https://cert.api.fiservapps.com/ch/sdk/v1/commercehub-client-sdk',
 		commercehubProdClient : 'https://prod.api.fiservapps.com/ch/sdk/v1/commercehub-client-sdk'
