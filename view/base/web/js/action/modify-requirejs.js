d1c83827c6988892389d9599c38c40432d6b7937
Ashwini Bhave <ashwini.bhave@fiserv.com>
2025-07-31 10:21:16 +0000

    MCH - modify require js added to fix issue with wrong paypal URLs

---
+++ view/base/web/js/action/modify-requirejs.js
@@ -0,0 +1,17 @@
+define([], function () {
+	'use strict';
+
+	return function (maps) {
+		let oldRequire = window.require;
+		window.require = (a,b,c) =>
+		{
+			if (typeof a === "object" && 
+				typeof a[0] !== "undefined" &&
+				typeof maps[a[0]] !== "undefined") {
+				a[0] = maps[a[0]];
+			}
+			return oldRequire(a,b,c);
+		}
+	};
+}
+);
