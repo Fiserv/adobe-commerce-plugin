a3708c39e3998e194af32d75cb136a5bf51ef75e
Nguyen <tyson.nguyen@fiserv.com>
2025-08-13 08:11:20 -0400

    COM ALT APM Config Setup
    COM alternative APM
    
    COM Fix system.xml

---
+++ Gateway/Config/Fastlane/Config.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Fiserv\Payments\Gateway\Config\PayPalFastLane;
+
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Framework\Serialize\Serializer\Json;
+
+/**
+ * Class Config
+ */
+class Config extends \Magento\Payment\Gateway\Config\Config
+{
+	// Constants for PayPal Fastlane configuration keys
+	const KEY_ACTIVE = 'active';
+	const KEY_PAYMENT_ACTION = 'payment_action';
+	const KEY_TITLE = 'title';
+
+	/**
+	 * @var \Magento\Framework\Serialize\Serializer\Json
+	 */
+	private $serializer;
+
+	/**
+	 * PayPal Fastlane Config constructor
+	 *
+	 * @param ScopeConfigInterface $scopeConfig
+	 * @param null|string $methodCode
+	 * @param string $pathPattern
+	 * @param Json|null $serializer
+	 */
+	public function __construct(
+		ScopeConfigInterface $scopeConfig,
+		$methodCode = null,
+		$pathPattern = self::DEFAULT_PATH_PATTERN,
+		Json $serializer = null
+	) {
+		parent::__construct($scopeConfig, $methodCode, $pathPattern);
+		$this->serializer = $serializer ?: \Magento\Framework\App\ObjectManager::getInstance()->get(Json::class);
+	}
+
+	/**
+	 * Gets PayPal Fastlane configuration status.
+	 *
+	 * @param int|null $storeId
+	 * @return bool
+	 */
+	public function isActive($storeId = null)
+	{
+		return (bool) $this->getValue(self::KEY_ACTIVE, $storeId);
+	}
+
+	/**
+	 * Gets value of PayPal Fastlane payment action.
+	 *
+	 * Possible values: Sale or Authorize.
+	 *
+	 * @param int|null $storeId
+	 * @return string
+	 */
+	public function getPaymentAction($storeId = null)
+	{
+		return $this->getValue(self::KEY_PAYMENT_ACTION, $storeId);
+	}
+
+	/**
+	 * Retrieve title for PayPal Fastlane section.
+	 *
+	 * @param int|null $storeId
+	 * @return array
+	 */
+	public function getPaymentMethodTitle($storeId = null)
+	{
+		return $this->getValue(self::KEY_TITLE, $storeId);
+	}
+}
