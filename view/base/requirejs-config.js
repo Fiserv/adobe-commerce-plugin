c61eb2ec4a9cb4068146b63864ee0bb2ef6e99ec
Nguyen <tyson.nguyen@fiserv.com>
2025-10-06 14:57:43 -0400

    COM SDK update

--- view/base/requirejs-config.js
+++ view/base/requirejs-config.js
@@ -14,7 +14,7 @@ var config = {
 		}
 	},
     paths : {
-		SDCv2Library : 'https://cdn-nonprod.fiservapps.com/ch/secure-data-capture/3.6.1/checkout',
+		SDCv2Library : 'https://cdn-nonprod.fiservapps.com/ch/secure-data-capture/3.5.3/checkout',
 		commercehubQaClient : 'https://qa.api.fiservapps.com/ch/sdk/v1/commercehub-client-sdk',
 		commercehubCertClient : 'https://cert.api.fiservapps.com/ch/sdk/v1/commercehub-client-sdk',
 		commercehubProdClient : 'https://prod.api.fiservapps.com/ch/sdk/v1/commercehub-client-sdk'
